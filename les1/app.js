db09feb71891c827897e2941f73a10a67390047e
Roy <34898@ma-web.nl>
2022-11-22 10:32:17 +0100

    kaasje

---
+++ les1/app.js
@@ -0,0 +1,24 @@
+class App
+{
+    runApplication()
+    {
+        console.log("Hello world!")
+        let appNaam = "Marco" 
+        let versienummer = 0.9
+        let versiedatum = Date
+        let autheur = "Roy" 
+        let copyright = "NCS"
+        let distributeur = "Marco"
+        let darkmode = true
+    }
+}
+console.log(appNaam)
+console.log(versienummer)
+console.log(autheur)
+console.log(copyright)
+console.log(distributeur)
+console.log(darkmode)
+console.log(versiedatum)
+
+let app = new App();
+app.runApplication();
